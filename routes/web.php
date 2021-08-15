c9b15bf0817a6db9c0d916fa55f7543d5f1e026b
b3rking <ishimweeschylebersy@gmail.com>
2021-08-15 11:25:30 +0200

    feat: auth routes

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * 
+ * -----------------------------
+ * Auth routes!  
+ * -----------------------------
+ */
+
+Route::get('/register', function() {
+    echo "register here!";
+});
+
+Route::get('/login', function() {
+    echo "login here!";
+});
+
+Route::get('/logout', function() {
+    echo "get out of here!";
+});
+
+/*
+Route::get('/forget-password', function() {
+    echo "register here!";
+});
+*/
