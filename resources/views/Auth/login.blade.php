a7d6572338e5bb92a6ff35b2a61bd2a77008b133
b3rking <ishimweeschylebersy@gmail.com>
2021-08-15 12:42:16 +0200

    implemented the login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -3,10 +3,14 @@
 @section('content')
     <div class="">
         <h1>Indentify yourself...</h1>
-        @if ( $errors->any() )
-            {{ $errors }}
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    {{ $error }}
+                @endforeach
+            </ul>
         @endif
-        <form action="auth" method="POST">
+        <form action="auth" method="GET">
             <input type="text" name="username" id="username">
             <input type="password" name="password" id="password">
             <button type="submit">Connection</button>
