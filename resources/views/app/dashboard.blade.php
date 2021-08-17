ab6398364b988423584d00ddc3b30ef3bd3700c2
b3rking <ishimweeschylebersy@gmail.com>
2021-08-17 08:27:16 +0200

    feat: working on roles

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -2,6 +2,11 @@
 @section('page', 'dashboard')
 @section('content')
 
-    you're logged in!
+    you're logged in! as {{ Auth::user()->username }}
+    @can('update', 'App\Models\User')
+        <p>ooh tu peux mettre a jour un user!</p>
+    @else
+        <p>non bro tu peux rien</p>
+    @endcan
     
 @endsection
