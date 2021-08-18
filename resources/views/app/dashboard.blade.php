2c5eda5ae854d5035c78df52fbf7a0db2981c177
b3rking <ishimweeschylebersy@gmail.com>
2021-08-18 08:12:51 +0200

    feat: showing the user role, small autorization check

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -3,10 +3,14 @@
 @section('content')
 
     you're logged in! as {{ Auth::user()->username }}
-    @can('update', User::class)
+    <ul>
+        <li>role => {{ Auth::user()->role->name }}</li>
+    </ul>
+
+    @if(Auth::user()->role_id === 1)
         <p>ooh tu peux mettre a jour un user!</p>
     @else
-        <p>non bro tu peux rien</p>
-    @endcan
+        <p>tu n'a aucun pouvoir :(</p>
+    @endif
     
 @endsection
