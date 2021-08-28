c0335a9ea6d1bf51eb6caf42999d0bc095fe1f32
b3rking <ishimweeschylebersy@gmail.com>
2021-08-28 12:08:19 +0200

    created the exits controller

---
+++ database/migrations/2021_08_28_100559_create_sorties_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSortiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sorties', function (Blueprint $table) {
+            $table->id();
+            $table->integer('client_id');
+            $table->integer('product_id');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('user_id');
+            $table->integer('empty');
+            $table->integer('deposit_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sorties');
+    }
+}
