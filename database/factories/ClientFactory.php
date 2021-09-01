85bbb3477e4989c53865f10d7553c4ee058f59b8
b3rking <ishimweeschylebersy@gmail.com>
2021-09-01 15:56:41 +0200

    recreated new data

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -24,6 +24,7 @@ class ClientFactory extends Factory
         return [
             'name' => $this->faker->company,
             'adress' => $this->faker->address,
+            'grade_id' => $this->faker->numberBetween(1, 4),
             'mobile_number' => $this->faker->phoneNumber()
         ];
     }
