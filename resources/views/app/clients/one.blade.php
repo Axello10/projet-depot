74c0c40a7d210089c0414ebf738fcb963d120671
b3rking <ishimweeschylebersy@gmail.com>
2021-09-03 10:05:52 +0200

    working reverse way

--- resources/views/app/clients/one.blade.php
+++ resources/views/app/clients/one.blade.php
@@ -4,5 +4,10 @@
 
         <h2> nom du client : {{ $client->name }} </h2>
         <p>{{ $client->adress }} | {{ $client->mobile_number }}</p>
+        <p>
+            @foreach ($client->empties as $cl)
+                {{ $cl->deposit->name }}
+            @endforeach
+        </p>
         
 @endsection
--- resources/views/app/empties/one.blade.php
+++ resources/views/app/empties/one.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 
         <h2> nom du client : {{ $empty->client->name }} </h2>
-        <p>nom du depot : {{ $empty->deposit_id }}</p>
         <p>nom du produit : {{ $empty->product->name }} || prix du produit : {{ $empty->product->price }}</p>
         <p>quantité : {{ $empty->quantity }}</p>
         <p>nom du depot : {{ $empty->deposit->name }}</p>
