36dad5aae565731de257e0aaefb54292e41581c2
b3rking <ishimweeschylebersy@gmail.com>
2021-09-05 11:47:26 +0200

    template for login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,19 +1,104 @@
-@extends('app')
-@section('page', 'Login')
-@section('content')
-    <div class="">
-        <h1>Indentify yourself...</h1>
-        @if ($errors->any())
-            <ul>
-                @foreach ($errors->all() as $error)
-                    {{ $error }}
-                @endforeach
-            </ul>
-        @endif
-        <form action="auth" method="GET">
-            <input type="text" name="username" id="username">
-            <input type="password" name="password" id="password">
-            <button type="submit">Connection</button>
-        </form>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>{{ $_ENV['APP_NAME'] }} - Page de connection</title>
+<!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+<!-- fin -->
+<!-- css supplémentaire -->
+    <style>
+        html,body {
+          height: 100%;
+        }
+
+        body {
+          display: flex;
+          align-items: center;
+          padding-top: 40px;
+          padding-bottom: 40px;
+          background-color: #f5f5f5;
+          color: #333;
+        }
+
+        .form-signin {
+          width: 100%;
+          max-width: 450px;
+          margin: auto;
+          
+        }
+
+        .form-signin .checkbox {
+          font-weight: 400;
+        }
+
+        .form-signin .form-floating:focus-within {
+          z-index: 2;
+          
+        }
+
+        .form-signin input[type="email"] {
+          margin-bottom: -1px;
+          border-bottom-right-radius: 0;
+          border-bottom-left-radius: 0;
+
+        }
+
+        .form-signin input[type="password"] {
+          margin-bottom: 10px;
+          border-top-left-radius: 0;
+          border-top-right-radius: 0;
+
+          
+        }
+        .form-signin form{
+            color: #999;
+            border-radius: 5px;
+            margin-bottom: 15px;
+            background: #fff;
+            box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.3);
+            padding: 40px;
+        }
+            .form-signin h1{
+              color: black;
+            }
+
+    </style>
+</head>
+
+  
+<body class="text-center">
+    
+    
+<main class="form-signin">
+    
+    @if ($errors->any())
+        <ul style="list-style: none">
+            @foreach ($errors->all() as $error)
+                <li style="color: red">{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+
+  <form action="auth" method="GET">
+    
+      <h1 class="mb-4">Se Connecter</h1>
+      <div class="form-floating">
+      <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com" name="username">
+      <label for="floatingInput">nom d'utilisateur</label>
     </div>
-@endsection
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="******" name="password">
+      <label for="floatingPassword">Mot de passe</label>
+    </div>
+    <br>
+    <button class="mb-2 w-100 btn btn-lg btn-primary " type="submit">Se connecter</button>
+  </form>
+</main>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+  </body>
+</html>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $_ENV['APP_NAME'] }} - @yield('page')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+
 <!-- fin -->
 <!-- css supplémentaire -->
     <style>
@@ -64,30 +69,153 @@
               color: black;
             }
 
+            <style>
+    .bd-placeholder-img {
+
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+
+      }
+    }
+    body {
+
+}
+
+.feather {
+  width: 16px;
+  height: 16px;
+  vertical-align: text-bottom;
+}
+
+/*
+ * Sidebar
+ */
+
+.sidebar {
+  position: fixed;
+  top: 0;
+  /* rtl:raw:
+  right: 0;
+  */
+  bottom: 0;
+  /* rtl:remove */
+  left: 0;
+  z-index: 100; /* Behind the navbar */
+  padding: 48px 0 0; /* Height of navbar */
+  box-shadow: inset 2px 2px 2px rgba(0, 0, 0, 0.3);
+}
+
+@media (max-width: 767.98px) {
+  .sidebar {
+    top: 5rem;
+  }
+}
+
+.sidebar-sticky {
+  position: relative;
+  top: 0;
+  height: calc(100vh - 48px);
+  padding-top: .5rem;
+  overflow-x: hidden;
+  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+}
+
+.sidebar .nav-link {
+  font-weight: 500;
+  color: #333;
+}
+
+.sidebar .nav-link .feather {
+  margin-right: 4px;
+ 
+}
+
+.sidebar .nav-link.active {
+  color: #2470dc;
+}
+
+.sidebar .nav-link:hover .feather,
+.sidebar .nav-link.active .feather {
+  color: inherit;
+}
+
+.sidebar-heading {
+
+  text-transform: uppercase;
+}
+
+/*
+ * Navbar
+ */
+
+.navbar-brand {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+  font-size: 1rem;
+  box-shadow: inset 2px 2px 2px rgba(0, 0, 0, 0.3);
+}
+
+.navbar .navbar-toggler {
+  top: .25rem;
+  right: 1rem;
+}
+
+.navbar .form-control {
+  padding: .75rem 1rem;
+  border-width: 0;
+  border-radius: 0;
+}
+
+.form-control-dark {
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: rgba(255, 255, 255, .1);
+}
+
+.form-control-dark:focus {
+
+  box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
+}
+
     </style>
 </head>
 <body>
+
+    <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="{{ route('dashboard') }}">{{ $_ENV['APP_NAME'] }}</a>
+        
+
+        @auth
+                <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+  
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+                    {{ Auth::user()->username }}
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><a class="dropdown-item" href="{{ route('dashboard') }}">dashboard</a></li>
+                        <li><a class="dropdown-item" href="{{ route('logout') }}">logout</a></li>
+                    </ul>
+                </div>
+            @endauth
+            @guest
+                <li><a href="{{ route('users.create') }}">new account</a></li>
+                <li><a href="{{ route('login') }}">login</a></li>
+            @endguest
+
+        </header>
     
-    <header>
-        <div class="">
-            <strong><p class="logo"><a href="{{ route('dashboard') }}">{{ $_ENV['APP_NAME'] }}</a></p></strong>
-        </div>
-        <div class="links">
-            <ul>
-                @auth
-                    <li><a href="{{ route('logout') }}">logout</a></li>
-                    <li><a href="{{ route('dashboard') }}">dashboard</a></li>
-                    <li>bienvenue, vous travaillez en tant que <em><strong>{{ Auth::user()->username }}</strong></em></li>
-                @endauth
-                @guest
-                    <li><a href="{{ route('users.create') }}">new account</a></li>
-                    <li><a href="{{ route('login') }}">login</a></li>
-                @endguest
-            </ul>
-        </div>
-        <hr>
-    </header>
     
     @yield('content')
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 </body>
 </html>
