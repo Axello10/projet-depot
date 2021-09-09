639fab0e937f89209193817df28de0ddc4f42a99
b3rking <ishimweeschylebersy@gmail.com>
2021-09-09 21:28:21 +0200

    working

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -9,31 +9,12 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
-
-
     public function products()
     {
-        $products = Product::OrderBy('created_at', 'desc')->get();
-
-        $entries = Entrie::all()->product;
-
-        // $total_products = [];
-        // foreach($products as $pd) {
-        //     $total_products[$pd->id] = Entrie::where('product_id', $pd->id)->get();
-        // }
+        $entries = Entrie::all();
         
-        // foreach ($total_products as $tp) {
-        //     for ($i = 0; $i < sizeof($tp); $i++) {
-        //         $data[$tp[$i]->id] = [
-        //             "quantité" => $tp[$i]->quantity,
-        //             "product_name" => Product::where('id', $tp[$i]->product_id)->get()
-        //         ];
-        //     }
-        // }
-
-        // return $data;
-        // return $fn;
-        // return [$data, $total_products];
-        // return [$products, $total_products];
+        foreach ($entries as $et) {
+            $products[] = $et->product;
+        }
     }
 }
