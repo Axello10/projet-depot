c4de70995f9018595e86f0db1bbbba7dbc06c108
b3rking <ishimweeschylebersy@gmail.com>
2021-09-09 21:17:12 +0200

    removed errors

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -14,22 +14,24 @@ class AppController extends Controller
     public function products()
     {
         $products = Product::OrderBy('created_at', 'desc')->get();
-        $total_products = [];
-        foreach($products as $pd) {
-            $total_products[$pd->id] = Entrie::where('product_id', $pd->id)->get();
-        }
+
+        $entries = Entrie::all()->product;
+
+        // $total_products = [];
+        // foreach($products as $pd) {
+        //     $total_products[$pd->id] = Entrie::where('product_id', $pd->id)->get();
+        // }
         
-        foreach ($total_products as $tp) {
-            for ($i = 0; $i < sizeof($tp); $i++) {
-                $data[$tp[$i]->id] = [
-                    "quantité" => $tp[$i]->quantity,
-                    "product_name" => Product::where('id', $tp[$i]->product_id)->get()
-                ];
-            }
-        }
+        // foreach ($total_products as $tp) {
+        //     for ($i = 0; $i < sizeof($tp); $i++) {
+        //         $data[$tp[$i]->id] = [
+        //             "quantité" => $tp[$i]->quantity,
+        //             "product_name" => Product::where('id', $tp[$i]->product_id)->get()
+        //         ];
+        //     }
+        // }
 
-
-        return $data;
+        // return $data;
         // return $fn;
         // return [$data, $total_products];
         // return [$products, $total_products];
