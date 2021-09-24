082b37935531b66a0f07e592b46a9860398cf849
b3rking <ishimweeschylebersy@gmail.com>
2021-09-24 08:38:50 +0200

    started with the controller

---
+++ app/Http/Controllers/App/DepositProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\App;
+
+use App\Http\Controllers\Controller;
+use App\Models\DepositProduct;
+use Illuminate\Http\Request;
+
+class DepositProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\DepositProduct  $depositProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(DepositProduct $depositProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\DepositProduct  $depositProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(DepositProduct $depositProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\DepositProduct  $depositProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, DepositProduct $depositProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\DepositProduct  $depositProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(DepositProduct $depositProduct)
+    {
+        //
+    }
+}
---
+++ database/factories/DepositProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\DepositProduct;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DepositProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = DepositProduct::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
