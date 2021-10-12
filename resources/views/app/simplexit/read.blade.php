174276d27aef4d22848d9071af59cbfd3ff9ee4c
b3rking <ishimweeschylebersy@gmail.com>
2021-10-12 22:20:33 +0200

    autorized sortie

--- resources/views/app/simplexit/read.blade.php
+++ resources/views/app/simplexit/read.blade.php
@@ -39,12 +39,13 @@
                 <a href="{{ route('simplexits.show', $st->id) }}"  class="btn btn-sm btn-primary mb-1 " >Plus de details</a>
 
                 <a href="{{ route('simplexits.edit', $st->id) }}"  class="btn btn-sm btn-info  mb-1">Modifier</a>
-                
+                @if(Auth::user()->role_id === 1)
                 <form action="{{ route('simplexits.destroy', $st->id) }}" method="POST">
                         @csrf
                         @method('DELETE')
                         <input type="submit" class="btn btn-sm btn-danger  mb-1" value="Supprimer">
                     </form>
+                    @endif
                 </div>
                 </td>
               </tr>
