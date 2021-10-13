714a6e625efef4bd1d90915268baec92d0d6203c
b3rking <ishimweeschylebersy@gmail.com>
2021-10-13 08:46:08 +0200

    adding final touch

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,153 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $_ENV['APP_NAME'] }} - @yield('page')</title>
-    <!-- Font Awesome -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
-  rel="stylesheet"
-/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
 
-<!-- Google Fonts -->
-<link
-  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-  rel="stylesheet"
-/>
-<!-- MDB -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css"
-  rel="stylesheet"
-/>
-<!-- MDB -->
-<script
-  type="text/javascript"
-  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"
-></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet" />
 
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
 
-<style>
+    <link rel="stylesheet" href="app.css">
 
-
-.feather {
-  width: 16px;
-  height: 16px;
-  vertical-align: text-bottom;
-}
-
-/*
- * Sidebar
- */
-
-.sidebar {
-  position: fixed;
-  top: 0;
- 
-  /* rtl:raw:
-  right: 0;
-  */
-  bottom: 0;
-  /* rtl:remove */
-  left: 0;
-  z-index: 100; /* Behind the navbar */
-  padding: 48px 0 0; /* Height of navbar */
-  box-shadow: inset 2px 2px 2px rgba(0, 0, 0, 0.3);
-}
-
-@media (max-width: 767.98px) {
-  .sidebar {
-    top: 5rem;
-  }
-}
-
-.position-sticky {
-  position: relative;
-  top: 0;
-  height: calc(100vh - 48px);
-  padding-top: .5rem;
-  overflow: auto;
-}
-
-.sidebar .nav-link {
-  font-weight: 500;
-  color: #333;
-  
-}
-
-.sidebar .nav-link .feather {
-  margin-right: 4px;
- 
-}
-
-.sidebar .nav-link.active {
-  color: #2470dc;
-}
-
-.sidebar .nav-link:hover .feather,
-.sidebar .nav-link.active .feather {
-  color: inherit;
-}
-
-.sidebar-heading {
-
-  text-transform: uppercase;
-}
-
-/*
- * Navbar
- */
-span{
-  font-size: 1.1rem;
-}
-.navbar-brand {
-  padding-top: .75rem;
-  padding-bottom: .75rem;
-  font-size: 1.1rem;
-}
-
-
-.navbar .navbar-toggler {
-  top: .25rem;
-  right: 1rem;
-}
-
-.navbar .form-control {
-  padding: .75rem 1rem;
-  border-width: 0;
-  border-radius: 0;
-}
-
-.form-control-dark {
-  color: #fff;
-  background-color: #0d6efd;
-  border-color: rgba(255, 255, 255, .1);
-}
-
-.form-control-dark:focus {
-
-  box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
-}
-
-.span_title2 {
-  font-size: 17px;text-transform: capitalize;
-}
-.nav-item span:hover{
-     cursor:pointer;
-     width:80%;
-     border-bottom: 2px  solid #0d6efd;
-     color: #0d6efd;
-}
-svg{
-  color: inherit;
-}
-  </style>
+    <!-- 
+       U  ___ u _____      _       _  __      _   _       _____  U _____ u    _      __  __   
+    \/"_ \/|_ " _| U  /"\  u  |"|/ /   U |"|u| |     |_ " _| \| ___"|/U  /"\  uU|' \/ '|u 
+    | | | |  | |    \/ _ \/   | ' /     \| |\| |       | |    |  _|"   \/ _ \/ \| |\/| |/ 
+.-,_| |_| | /| |\   / ___ \ U/| . \\u    | |_| |      /| |\   | |___   / ___ \  | |  | |  
+ \_)-\___/ u |_|U  /_/   \_\  |_|\_\    <<\___/      u |_|U   |_____| /_/   \_\ |_|  |_|  
+      \\   _// \\_  \\    >>,-,>> \\,-.(__) )(       _// \\_  <<   >>  \\    >><<,-,,-.   
+     (__) (__) (__)(__)  (__)\.)   (_/     (__)     (__) (__)(__) (__)(__)  (__)(./  \.)      
+    !-->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    {{-- stoping site from getting indexed --}}
+    <meta name="robots" content="noindex, nofollow">
+    {{-- done --}}
 </head>
 <body>
     @auth
     <header class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow ">
         <a class="navbar-brand col-md-3 col-lg-2  me-0 px-3" href="{{ route('dashboard') }}">{{ $_ENV['APP_NAME'] }}</a>
-        
-
-       
-                
+                        
                 <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -164,9 +51,7 @@ svg{
                         <li><a class="dropdown-item" href="{{ route('logout') }}">logout</a></li>
                     </ul>
                 </div>
-        
-
-
+    
         </header>
     
     <div class="container-fluid">
